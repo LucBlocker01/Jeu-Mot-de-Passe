cfde4b7a7df2f63f620dfc0bb816ca16b164efc4
LAPTOP-QMITUAI4\hluca <lucas.hureaux@etudiant.univ-reims.fr>
2024-11-29 11:50:35 +0100

    simplify rule check functions

--- src/ruleFunctions.js
+++ src/ruleFunctions.js
@@ -1,22 +1,13 @@
 const Rule1 = (passV) => {
-  if (passV.length >= 8) {
-    return true;
-  }
-  return false;
+  return passV.length >= 8;
 }
 
 const Rule2 = (passV) => {
-  if (passV.toLowerCase() !== passV) {
-    return true;
-  }
-  return false;
+  return passV.toLowerCase() !== passV;
 }
 
 const Rule3 = (passV) => {
-  if (/[1-9]/g.test(passV)) {
-    return true;
-  }
-  return false;
+  return /[1-9]/g.test(passV);
 }
 
 const rulesFunc = [Rule1, Rule2, Rule3];
