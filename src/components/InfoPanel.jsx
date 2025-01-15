5556eb4013b4ecbe91f46b85e0454c649416b25c
LAPTOP-QMITUAI4\hluca <lucas.hureaux@etudiant.univ-reims.fr>
2025-01-15 11:53:57 +0100

    hide and add fadein to the infos

--- src/components/InfoPanel.jsx
+++ src/components/InfoPanel.jsx
@@ -13,17 +13,17 @@ function InfoPanel({passV, length}) {
         <div class={`panel ${length >= 5 ? "visible" : ""}`}>
             <div class="panelHeader">Informations du mot de passe</div>
             <div class="panelMain">
-                <div class="info">
+                <div class={`info ${length >= 5 ? "visible" : ""}`}>
                     <div class="infoHeader">Somme des chiffres</div>
                     <div class="infoMain">{passSum}</div>
                 </div>
-                <div class="info">
-                    <div class="infoHeader">Longueur</div>
-                    <div class="infoMain">{passVLength}</div>
-                </div>
-                <div class="info">
+                <div class={`info ${length >= 11 ? "visible" : ""}`}>
                     <div class="infoHeader">Produit des nombres romains</div>
                     <div class="infoMain">{passRomanProduct}</div>
+                </div>
+                <div class={`info ${length >= 13 ? "visible" : ""}`}>
+                    <div class="infoHeader">Longueur</div>
+                    <div class="infoMain">{passVLength}</div>
                 </div>
             </div>
         </div>
